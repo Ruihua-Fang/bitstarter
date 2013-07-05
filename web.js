1629895db3ce3cbc54d695e42bb192f0aebd9c04
Ubuntu <ubuntu@ip-172-31-25-152.us-west-2.compute.internal>
2013-07-05 22:50:27 +0000

    testing

--- web.js
+++ web.js
@@ -2,13 +2,13 @@ var express = require('express');
 var fs = require('fs');
 var infile = 'index.html';
 var data = fs.readFileSync(infile, 'utf8');
-console.log('test_data'+ data);
+console.log('test_data'+ data.toString());
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
    response.send("temp");
-   response.send(data);
+   response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
